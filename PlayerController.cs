808e16deac8bdb9c291860e0e129025277b32221
megamell <44287801+megamell@users.noreply.github.com>
2020-11-21 11:33:34 +0100

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed1;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+
+    void Update()
+    {
+        //inpuuuuut
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //draaien en vooooooruitt met die schuit
+        transform.Translate(Vector3.forward * Time.deltaTime  * speed1 * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
